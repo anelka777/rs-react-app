56fd54ef8ba8bde80aede06ef4c616673c857665
anelka777 <46974606+anelka777@users.noreply.github.com>
2026-06-06 13:23:32 -0700

    feat: add modal component with react portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import { type ReactElement } from 'react';
 
+import Modal from './components/Modal/Modal';
+
 const App = (): ReactElement => {
-  return <div>App</div>;
+  const [isUncontrolledOpen, setIsUncontrolledOpen] = useState(false);
+  const [isHookFormOpen, setIsHookFormOpen] = useState(false);
+
+  return (
+    <div>
+      <h1>React Forms</h1>
+      <button onClick={() => setIsUncontrolledOpen(true)}>
+        Uncontrolled Form
+      </button>
+      <button onClick={() => setIsHookFormOpen(true)}>React Hook Form</button>
+
+      <Modal
+        isOpen={isUncontrolledOpen}
+        onClose={() => setIsUncontrolledOpen(false)}
+        title="Uncontrolled Form"
+      >
+        <p>Uncontrolled form will be here</p>
+      </Modal>
+
+      <Modal
+        isOpen={isHookFormOpen}
+        onClose={() => setIsHookFormOpen(false)}
+        title="React Hook Form"
+      >
+        <p>Hook form will be here</p>
+      </Modal>
+    </div>
+  );
 };
 
 export default App;
