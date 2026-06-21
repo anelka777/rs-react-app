8275f6d12679d6df31ac23bcecc996b7baad651d
anelka777 <46974606+anelka777@users.noreply.github.com>
2026-06-20 22:53:44 -0700

    feat: add server action for CSV export, remove useCsvExport hook

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -3,8 +3,8 @@
 import { type JSX } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../store/store';
-import useCsvExport from '../../hooks/useCsvExport';
 import { unselectAll } from '../../store/selectedSlice';
+import { generateCsvAction } from '../../actions/csvActions';
 
 import styles from './Flyout.module.css';
 
@@ -12,14 +12,23 @@ const Flyout = (): JSX.Element | null => {
   const dispatch = useAppDispatch();
   const items = useAppSelector((state) => state.selectedCharacters.items);
 
-  const { handleDownload } = useCsvExport(items);
-
   if (items.length === 0) {
     return null;
   }
 
   const handleUnselectAll = (): void => {
     dispatch(unselectAll());
+  };
+
+  const handleDownload = async (): Promise<void> => {
+    const csv = await generateCsvAction(items);
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${items.length}_items.csv`;
+    a.click();
+    URL.revokeObjectURL(url);
   };
 
   return (
